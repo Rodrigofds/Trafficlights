938fd838459cde844d9b314d7b0299e710f18f79
Rodrigofds <rodrigoldiscool@gmail.com>
2020-10-02 18:05:36 -0300

    Pt-Br = Adiciona comentários. En = Added comments.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,27 +1,27 @@
-function vermelha(){
+function vermelha(){ // That set  the red color in the traffic light and wait X seconds to call the next function.
     off();
     document.getElementById('vermelha').classList.add('vermelha');
     setTimeout(verde, 5000);
 }
 
-function amarela(){
+function amarela(){ // That set the yellow color in the traffic light and wait X seconds to call the next function.
     off();
     document.getElementById('amarela').classList.add('amarela');
     setTimeout(vermelha, 2500);
 }
 
-function verde(){
+function verde(){ // That set the green color in the traffic light and wait X seconds to call the next function.
     off();
     document.getElementById('verde').classList.add('verde');
     setTimeout(amarela, 5000);
 }
 
-function off(){
+function off(){ //That clear all colors in the traffic light,
     document.getElementById('vermelha').classList.remove('vermelha');
     document.getElementById('amarela').classList.remove('amarela');
     document.getElementById('verde').classList.remove('verde');
 }
 
-function iniciar(){
+function iniciar(){ // That initialize the traffic light.
     vermelha();
 }
